c8dbfa2cf15c6654e7b85c7ae3f414ef4a9f9070
Sih-Jin Lee <st10451904@imconnect.edu.za>
2025-09-17 03:21:42 +0200

    2 commit
    Create the view folders

--- ContractMonthlyClaimSystem/obj/Debug/net9.0/ContractMonthlyClaimSystem.AssemblyInfo.cs
+++ ContractMonthlyClaimSystem/obj/Debug/net9.0/ContractMonthlyClaimSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContractMonthlyClaimSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b81490b5b3ed7736b3d400a023a130084208c74")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContractMonthlyClaimSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContractMonthlyClaimSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
